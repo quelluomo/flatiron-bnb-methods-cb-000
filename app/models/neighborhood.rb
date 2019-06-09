dd16babc95ac59063aac7bfffd4fde195d46d8a1
Matteo Muscas <thatman.j@gmail.com>
2019-06-09 14:36:57 +0000

    Automatically backed up by Learn

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -3,5 +3,12 @@ class Neighborhood < ActiveRecord::Base
   extend Helpers::ClassMethods
   belongs_to :city
   has_many :listings
+  has_many :reservations, :through => :listings
+
+  include Helpers
+
+  def city_openings(start_date, end_date)
+    openings(start_date, end_date)
+  end
 
 end
