8528cd4ffa68a576bc21c6bc90240e8614cab31b
Matteo Muscas <thatman.j@gmail.com>
2019-06-09 14:41:03 +0000

    Automatically backed up by Learn

--- app/models/concerns/helpers.rb
+++ app/models/concerns/helpers.rb
@@ -16,16 +16,16 @@ module Helpers
   module ClassMethods
 
     def highest_ratio_res_to_listings
-        self.has_listings.max_by {|location| location.reservations.count / location.listings.count }
-      end
+      self.has_listings.max_by {|location| location.reservations.count / location.listings.count }
+    end
 
-      def most_res
-        self.has_listings.max_by {|location| location.reservations.count }
-      end
+    def most_res
+      self.has_listings.max_by {|location| location.reservations.count }
+    end
 
-      def has_listings
-        self.all.select {|location| location.listings.any? }
-      end
+    def has_listings
+      self.all.select {|location| location.listings.any? }
+    end
 
   end
 end
