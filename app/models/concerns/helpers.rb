e4b3c03eee4dc089fb5c6974a78e4d24b839c24b
Matteo Muscas <thatman.j@gmail.com>
2019-06-09 14:21:08 +0000

    Automatically backed up by Learn

--- app/models/concerns/helpers.rb
+++ app/models/concerns/helpers.rb
@@ -1,2 +1,15 @@
 module Helpers
   extend ActiveSupport::Concern
+
+  def openings(start_date, end_date)
+     listings.merge(Listing.available(start_date, end_date))
+   end
+
+   def ratio_reservations_to_listings
+     if listings.count > 0
+       reservations.count.to_f / listings.count.to_f
+     end
+   end
+
+ end
+ end
