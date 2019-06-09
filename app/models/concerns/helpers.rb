b533b8a1337f2c8bd46b0e0795535980f452ae06
Matteo Muscas <thatman.j@gmail.com>
2019-06-09 14:23:33 +0000

    Automatically backed up by Learn

--- app/models/concerns/helpers.rb
+++ app/models/concerns/helpers.rb
@@ -2,14 +2,28 @@ module Helpers
   extend ActiveSupport::Concern
 
   def openings(start_date, end_date)
-     listings.merge(Listing.available(start_date, end_date))
-   end
+    listings.merge(Listing.available(start_date, end_date))
+  end
 
-   def ratio_reservations_to_listings
-     if listings.count > 0
-       reservations.count.to_f / listings.count.to_f
-     end
-   end
+  def ratio_reservations_to_listings
+    if listings.count > 0
+      reservations.count.to_f / listings.count.to_f
+    end
+  end
 
- end
- end
+  class_methods do
+
+    def highest_ratio_reservations_to_listings
+
+      all.max do |a, b|
+        a.ratio_reservations_to_listings <=> b.ratio_reservations_to_listings
+      end
+    end
+
+    def most_reservations
+      all.max do |a, b|
+        a.reservations.count <=> b.reservations.count
+      end
+    end
+  end
+end
