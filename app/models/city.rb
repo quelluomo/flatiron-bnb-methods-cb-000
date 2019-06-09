b8f28b4065de34a5530b6649edab63f236423010
Matteo Muscas <thatman.j@gmail.com>
2019-06-09 14:24:27 +0000

    Automatically backed up by Learn

--- app/models/city.rb
+++ app/models/city.rb
@@ -3,6 +3,11 @@ class City < ActiveRecord::Base
   has_many :listings, :through => :neighborhoods
   has_many :reservations, :through => :listings
 
+  include Helpers
+
+  def city_openings(start_date, end_date)
+    openings(start_date, end_date)
+  end
 
 
 end
