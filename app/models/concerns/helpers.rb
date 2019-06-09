c9dad618acc7521f5cd4497f0e8117274139772c
Matteo Muscas <thatman.j@gmail.com>
2019-06-09 14:40:32 +0000

    Automatically backed up by Learn

--- app/models/concerns/helpers.rb
+++ app/models/concerns/helpers.rb
@@ -15,22 +15,17 @@ module Helpers
 
   module ClassMethods
 
-  def ratio_reservations_to_listings
-    if listings.count > 0
-      reservations.count.to_f / listings.count.to_f
-    end
-  end
+    def highest_ratio_res_to_listings
+        self.has_listings.max_by {|location| location.reservations.count / location.listings.count }
+      end
 
-  def highest_ratio_reservations_to_listings
-    all.max do |a, b|
-      a.ratio_reservations_to_listings <=> b.ratio_reservations_to_listings
-    end
-  end
+      def most_res
+        self.has_listings.max_by {|location| location.reservations.count }
+      end
 
-  def most_reservations
-    all.max do |a, b|
-      a.reservations.count <=> b.reservations.count
-    end
-  end
+      def has_listings
+        self.all.select {|location| location.listings.any? }
+      end
+
   end
 end
