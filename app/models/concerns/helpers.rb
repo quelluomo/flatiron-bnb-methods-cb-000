3257e3c60a8fc195d88d520e68615d8284a65fc4
Matteo Muscas <thatman.j@gmail.com>
2019-06-09 14:41:49 +0000

    Automatically backed up by Learn

--- app/models/concerns/helpers.rb
+++ app/models/concerns/helpers.rb
@@ -1,14 +1,14 @@
 module Helpers
   module InstanceMethods
 
-  def openings(start_date, end_date)
-    openings = []
-      self.listings.each do |listing|
-        if listing.reservations.none? { |r|
-          (start_date.to_date >= r.checkin && start_date.to_date <= r.checkout) || (end_date.to_date <= r.checkout && end_date.to_date >= r.checkin)}
-        openings << listing
-      end
-    end
+    def openings(start_date, end_date)
+      openings = []
+        self.listings.each do |listing|
+          if listing.reservations.none? { |r|
+            (start_date.to_date >= r.checkin && start_date.to_date <= r.checkout) || (end_date.to_date <= r.checkout && end_date.to_date >= r.checkin)}
+            openings << listing
+          end
+        end
       openings
     end
   end
